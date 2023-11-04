828d276cda76d200481bb8f9ba96a756d6b860fd
Madeleine Woodbury <mail@woodburycode.com>
2023-11-04 17:24:53 -0400

    Inital commit

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+// Initialize Ammo.js
+Ammo().then(async (AmmoLib) => {
+  Ammo = AmmoLib
+  console.log(Ammo)
+
+  // Initialize the environment
+  console.log('environment')
+
+  // Start
+  console.log('start')
+})
